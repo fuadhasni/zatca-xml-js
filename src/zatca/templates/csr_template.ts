f341145a236003c6de210e67be84faa456497f6c
fuadhasni <fuad@infosun.co.uk>
2023-02-20 15:31:31 +0500

    csr correction

--- src/zatca/templates/csr_template.ts
+++ src/zatca/templates/csr_template.ts
@@ -16,14 +16,21 @@ utf8 = no
 distinguished_name = my_req_dn_prompt
 
 # Extensions
-req_extensions = v3_req
+#req_extensions = v3_req
 
-[ v3_req ]
-basicConstraints=CA:FALSE
-#keyUsage = digitalSignature, nonRepudiation, keyEncipherment
+[v3_req]
+basicConstraints = CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment
+[req_ext]
+certificateTemplateName = ASN1:PRINTABLESTRING:SET_PRODUCTION_VALUE
+subjectAltName = dirName:alt_names
+
+#[ v3_req ]
+#basicConstraints=CA:FALSE
+#keyUsage = digitalSignature, keyEncipherment
 # Production or Testing Template (TSTZATCA-Code-Signing - ZATCA-Code-Signing)
-1.3.6.1.4.1.311.20.2 = ASN1:UTF8String:SET_PRODUCTION_VALUE
-subjectAltName=dirName:dir_sect
+#1.3.6.1.4.1.311.20.2 = ASN1:UTF8String:SET_PRODUCTION_VALUE
+#subjectAltName=dirName:dir_sect
 
 [ dir_sect ]
 # EGS Serial number (1-SolutionName|2-ModelOrVersion|3-serialNumber)
